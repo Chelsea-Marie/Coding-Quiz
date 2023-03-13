5b542ff51b5a278e90a92a456b769c41b25e4b48
chel.c <chelsea.marie.pederson@gmail.com>
2023-03-13 18:01:34 -0500

    finished code

--- script.js
+++ script.js
@@ -1,69 +1,82 @@
 var startPage = document.getElementById("start-page");
 var questionPage = document.getElementById("question-page");
 var endPage = document.getElementById("end-page");
+var scorePage = document.getElementById("highscore-page");
 var startButton = document.getElementById("start-btn");
-var questionText = document.getElementById("question-text")
-var option1Btn = document.getElementById("option-1")
-var option2Btn = document.getElementById("option-2")
-var option3Btn = document.getElementById("option-3")
-var option4Btn = document.getElementById("option-4")
-
+var questionText = document.getElementById("question-text");
+var option1Btn = document.getElementById("option-1");
+var option2Btn = document.getElementById("option-2");
+var option3Btn = document.getElementById("option-3");
+var option4Btn = document.getElementById("option-4");
+var resultP = document.getElementById("result");
+var finalScoreSpan = document.getElementById("final-score");
+var initialsInput = document.getElementById("initials-input");
+var submitBtn = document.getElementById("submit-btn");
+var hsBtn = document.getElementById("hs-btn");
+var scoreListUl = document.getElementById("score-list");
+var timeId;
+var totalTime = 60;
 var currentIndex = 0;
+
+var savedScores = JSON.parse(localStorage.getItem("savedScore")) || [];
 
 var questions = [
     {
-       text: "What is 1 + 1?" ,
+       text: "What option will make the largest heading size?" ,
        options: [
-        "3", "4", "2","5"
+        "h3", "h4", "h1","h2"
        ],
-       answer: "2"
+       answer: "h1"
     },
     {
-        text: "What is 1 + 2?" ,
+        text: "How are webpages styled?" ,
         options: [
-         "three", "four", "two","five"
+         "HTML", "CSS", "Python","Node.js"
         ],
-        answer: "three"
+        answer: "CSS"
      },
      {
-        text: "What is 1 + 1?" ,
+        text: "How do you call an id in CSS?" ,
         options: [
-         "3", "4", "2","5"
+         ".", "{}", "*","#"
         ],
-        answer: "2"
+        answer: "#"
      },
      {
-         text: "What is 1 + 2?" ,
+         text: "What was JavaScript's original name?" ,
          options: [
-          "three", "four", "two","five"
+          "Mocha", "Espresso", "Latte", "Coffee"
          ],
-         answer: "three"
+         answer: "Mocha"
       },
       {
-        text: "What is 1 + 1?" ,
+        text: "What is plain JavaScript without any added libraries or frameworks?" ,
         options: [
-         "3", "4", "2","5"
+         "Chocolate JavaScript", "Strawberry JavaScript", "Cinnamon JavaScript","Vanilla JavaScript"
         ],
-        answer: "2"
+        answer: "Vanilla JavaScript"
      },
      {
-         text: "What is 1 + 2?" ,
+         text: "What is a coding error in a computer program?" ,
          options: [
-          "three", "four", "two","five"
+          "Ant", "Bug", "Squirrel", "Pest"
          ],
-         answer: "three"
+         answer: "Bug"
       }
 ]
 
 function startQuiz() {
-    startPage.classList.add("hide");
+    startPage.classList.add("hide")
     questionPage.classList.remove("hide");
+    timeId=setInterval(timeInterval, 1000)
     displayQuestion();
 }
 
 function endQuiz () {
+    clearInterval(timeId);
     questionPage.classList.add("hide");
     endPage.classList.remove("hide");
+    finalScoreSpan.textContent = totalTime;
 }
 
 function displayQuestion() {
@@ -74,7 +87,23 @@ function displayQuestion() {
     option4Btn.textContent = questions[currentIndex].options[3]
 }
 
-function nextQuestion() {
+function nextQuestion(event) {
+    console.log(event.target);
+    console.log(event.target.textContent);
+    console.log(questions[currentIndex].answer)
+    
+    if(event.target.textContent == questions[currentIndex].answer) {
+        console.log("correct")
+        resultP.textContent = "Correct!"
+    } else {
+        console.log("wrong")
+        resultP.textContent = "Wrong!"
+        totalTime = totalTime - 10;
+
+    }
+
+
+
     currentIndex++;
     if(currentIndex < questions.length) {
         displayQuestion();
@@ -83,8 +112,50 @@ function nextQuestion() {
     }
 }
 
+function saveScore() {
+    var initials = initialsInput.value;
+    var score = totalTime;
+
+    var data = {
+        initials,
+        score
+    }
+
+    savedScores.push(data)
+
+    localStorage.setItem("savedScore", JSON.stringify(savedScores))
+}
+
+function showHighscores () {
+    endPage.classList.add("hide");
+    scorePage.classList.remove("hide");
+
+    var savedData = JSON.parse(localStorage.getItem("savedScore"));
+
+    for(i = 0; i < savedData.length; i++) {
+        var newLi = document.createElement("li");
+        newLi.textContent = savedData[i].initials + " - " + savedData[i].score
+        scoreListUl.append(newLi)
+    }
+
+
+
+
+}
+
+function timeInterval() {
+    document.getElementById('timer').textContent = totalTime;
+    totalTime = totalTime - 1;
+    if(totalTime <= 0){
+        clearInterval(timeId);
+        endQuiz();
+    }
+}
+
 startButton.addEventListener("click", startQuiz)
 option1Btn.addEventListener("click", nextQuestion)
 option2Btn.addEventListener("click", nextQuestion)
 option3Btn.addEventListener("click", nextQuestion)
 option4Btn.addEventListener("click", nextQuestion)
+submitBtn.addEventListener("click", saveScore)
+hsBtn.addEventListener("click", showHighscores)
